ea7ae85c0593a6af1172357f735eaf1785283ed4
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2023-10-02 12:26:37 +0200

    v2

---
+++ conf/telemetry_settings.php
@@ -0,0 +1,17 @@
+<?php
+
+// Type of db: "mysql", "sqlite" or "postgresql"
+$db_type = 'mysql';
+// Password to login to stats.php. Change this!!!
+$stats_password = '__PASSWORD__';
+// If set to true, test IDs will be obfuscated to prevent users from guessing URLs of other tests
+$enable_id_obfuscation = false;
+// If set to true, IP addresses will be redacted from IP and ISP info fields, as well as the log
+$redact_ip_addresses = false;
+
+// Mysql settings
+$MySql_username = '__DB_USER__';
+$MySql_password = '__DB_PWD__';
+$MySql_hostname = 'localhost';
+$MySql_databasename = '__DB_NAME__';
+$MySql_port = '3306';
---
+++ doc/DESCRIPTION_fr.md
@@ -0,0 +1,12 @@
+Test de vitesse auto-hébergé pour HTML5 et plus. Configuration facile, exemples, configurable, adapté aux appareils mobiles. Prend en charge PHP, Node, plusieurs serveurs, etc.
+
+### Caractéristiques
+
+- Télécharger
+- Télécharger
+-pinger
+- Gigue
+- Adresse IP, FAI, distance du serveur (facultatif)
+- Télémétrie (facultatif)
+- Partage des résultats (facultatif)
+- Plusieurs points de test (facultatif)
